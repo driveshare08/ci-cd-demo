29d177ba451887b671e724862dfea94ec62bff83
driveshare08 <driveshare08@gmail.com>
2026-03-30 13:22:36 +0100

    Add outputs for resource group, AKS, and ACR

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "client_id" {
+  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
